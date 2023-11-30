98cec80a087e3ebeecebf2949d0888f5fc346ba4
fbrcode <fabio.bressler@gmail.com>
2023-11-29 22:19:22 -0300

    feat: enhance with dynamo db stack

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,8 +1,26 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  Table as DynamoTable,
+  AttributeType,
+  ITable,
+} from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
+import { getSuffixFromStack } from "../Utils";
 
 export class DataStack extends Stack {
+  public readonly spacesTable: ITable;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.spacesTable = new DynamoTable(this, "SpacesTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: `SpacesTable-${suffix}`,
+    });
   }
 }
--- src/services/hello.js
+++ src/services/hello.js
@@ -1,6 +1,8 @@
 exports.main = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, Lambda World" }),
+    body: JSON.stringify({
+      message: `Hello, I will read from ${process.env.TABLE_NAME}`,
+    }),
   };
 };
