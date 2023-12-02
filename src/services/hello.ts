3c4c619031e368dc2521404d76fb48baf1f4651a
fbrcode <fabio.bressler@gmail.com>
2023-12-02 07:25:32 -0300

    feat: read s3 buckets from lambda

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -4,15 +4,25 @@ import {
   Context,
 } from "aws-lambda";
 import { v4 } from "uuid";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+// const s3Client = new S3Client({ region: "us-east-1" });
+const s3Client = new S3Client({});
 
 export async function handler(event: APIGatewayProxyEvent, context: Context) {
   console.log(event);
+
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify({
       message: `Hello, I will read from ${
         process.env.TABLE_NAME
-      } with id ${v4()}`,
+      } with id ${v4()} and here are the buckets list: ${JSON.stringify(
+        listBucketsResult
+      )}`,
     }),
   };
 
