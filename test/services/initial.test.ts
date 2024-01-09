aeaeedccf2563e09149be82a5444bf8232542340
fbrcode <fabio.bressler@gmail.com>
2024-01-09 16:43:35 -0300

    test: init services test

---
+++ test/services/initial.test.ts
@@ -0,0 +1,8 @@
+describe("Initial test suite", () => {
+  test("Initial test", () => {
+    // 1.arrange (prepare)
+    // 2.act (execute)
+    // 3.assert (verify)
+    expect(true).toBeTruthy();
+  });
+});
