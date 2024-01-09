a4fc3f7e51faa1875da9e82a4456a01bb7c6221d
fbrcode <fabio.bressler@gmail.com>
2024-01-09 09:38:05 -0300

    test: add ref template tests

--- test/infra/MonitorStack.test.ts
+++ test/infra/MonitorStack.test.ts
@@ -1,6 +1,6 @@
 import { App } from "aws-cdk-lib";
 import { MonitorStack } from "../../src/infra/stacks/MonitorStack";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 
 describe("CDK test suite", () => {
   let monitorStackTemplate: Template;
@@ -29,6 +29,44 @@ describe("CDK test suite", () => {
     monitorStackTemplate.hasResourceProperties("AWS::SNS::Topic", {
       DisplayName: "AlarmTopic",
       TopicName: "AlarmTopic",
+    });
+  });
+
+  test("Sns subscription properties - with matchers", () => {
+    // 2.act (execute) - query for the construct
+    // 3.assert (verify) - check construct properties
+    monitorStackTemplate.hasResourceProperties(
+      "AWS::SNS::Subscription",
+      Match.objectEquals({
+        Protocol: "lambda",
+        TopicArn: {
+          Ref: Match.stringLikeRegexp("AlarmTopic*"),
+        },
+        Endpoint: {
+          "Fn::GetAtt": [Match.stringLikeRegexp("webHookLambda*"), "Arn"],
+        },
+      })
+    );
+  });
+
+  test("Sns subscription properties - with exact values", () => {
+    // 2.act (execute) - query for the construct
+    // 3.assert (verify) - check construct properties
+
+    const snsTopic = monitorStackTemplate.findResources("AWS::SNS::Topic");
+    const snsTopicName = Object.keys(snsTopic)[0];
+
+    const lambda = monitorStackTemplate.findResources("AWS::Lambda::Function");
+    const lambdaName = Object.keys(lambda)[0];
+
+    monitorStackTemplate.hasResourceProperties("AWS::SNS::Subscription", {
+      Protocol: "lambda",
+      TopicArn: {
+        Ref: snsTopicName,
+      },
+      Endpoint: {
+        "Fn::GetAtt": [lambdaName, "Arn"],
+      },
     });
   });
 });
