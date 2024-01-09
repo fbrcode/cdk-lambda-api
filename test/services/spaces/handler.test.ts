65af219f50796cacfa645547aa47ca2db33fb778
fbrcode <fabio.bressler@gmail.com>
2024-01-09 19:13:49 -0300

    test: test handler client outside function call

---
+++ test/services/spaces/handler.test.ts
@@ -0,0 +1,44 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { handler } from "../../../src/services/spaces/handler";
+
+const someItems = [
+  {
+    id: {
+      S: "123",
+    },
+    location: {
+      S: "Paris",
+    },
+  },
+];
+
+// mock the DynamoDBClient constructor (mock the client and the send method)
+// ugly but effective approach
+jest.mock("@aws-sdk/client-dynamodb", () => {
+  return {
+    DynamoDBClient: jest.fn().mockImplementation(() => {
+      return {
+        send: jest.fn().mockImplementation(() => {
+          return {
+            Items: someItems,
+          };
+        }),
+      };
+    }),
+    ScanCommand: jest.fn(),
+  };
+});
+
+describe("Spaces handler test suite", () => {
+  test("Returns spaces from dynamoDb", async () => {
+    const result = await handler({ httpMethod: "GET" } as any, {} as any);
+    expect(result.statusCode).toBe(200);
+    const expectedResult = [{ id: "123", location: "Paris" }];
+
+    const parsedResultBody = JSON.parse(result.body);
+    expect(parsedResultBody).toEqual(expectedResult);
+
+    expect(DynamoDBClient).toHaveBeenCalledTimes(1);
+    expect(ScanCommand).toHaveBeenCalledTimes(1);
+  });
+});
